d3ee93216a3f2a92e091c4bfd3c592b9e0eac83b
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-02 16:27:35 +0100

    Ajout de la fonctionnalité de recherche d'un pays

--- app/src/main/java/com/android/mycountryapp/presentation/CountriesList.kt
+++ app/src/main/java/com/android/mycountryapp/presentation/CountriesList.kt
@@ -1,4 +1,5 @@
 package com.android.mycountryapp.presentation
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -21,122 +22,75 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.android.mycountryapp.data.models.CountriesData
 import com.android.mycountryapp.ui.theme.MyGreen
+import com.android.mycountryapp.ui.theme.SoftGray
 import com.android.mycountryapp.utils.Screen
-import com.android.mycountryapp.viewmodel.CountryViewModel
 import com.android.mycountryapp.viewmodel.states.CountryState
 
-//@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CountriesList(
-    countryModel: CountryViewModel,
+    countriesState: CountryState,
     navController: NavController
 ) {
 
-    val countriesState = countryModel.countriesState.collectAsState().value
-
-    val pageSize: Int = (250 + 8) / 9
     var actualPage by remember { mutableStateOf(0) }
+    var actualPageWithSearch by remember { mutableStateOf(0) }
+    var pageSize by remember { mutableStateOf(1) }
+    var searchKey by remember { mutableStateOf("") }
+
 
     Scaffold(
 
             topBar = {
-
-                TopAppBar()
+                TopAppBar(
+                        updateActualPageWithSearch = { actualPageWithSearch = it },
+                        updateSearchKey = { searchKey = it },
+                        searchKey = searchKey
+                )
             },
 
             content = { innerPadding ->
 
                 BodyList(
-                        innerPadding ,
-                        countryModel ,
-                        countriesState ,
-                        navController,
-                        actualPage
+                        innerPadding = innerPadding,
+                        countriesState = countriesState,
+                        navController = navController,
+                        actualPage = if(searchKey.isEmpty()) actualPage else actualPageWithSearch,
+                        searchKey = searchKey,
+                        updatePageSize = { pageSize = it }
                 )
             },
 
             bottomBar = {
-                Row(
-                        modifier = Modifier
-                            .background(Color.White)
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically
-                ) {
 
-                    IconButton(
-                            onClick = { actualPage = 0 },
-                            modifier = Modifier
-                                .weight(1.5f),
-                            enabled = actualPage != 0
-                    ) {
-                        Icon(
-                                Icons.Rounded.SkipPrevious,
-                                contentDescription = "",
-                        )
-                    }
-
-                    IconButton(
-                            onClick = { actualPage-- },
-                            modifier = Modifier
-                                .weight(1.5f),
-                            enabled = actualPage != 0
-                    ) {
-                        Icon(
-                                Icons.Rounded.KeyboardArrowLeft,
-                                contentDescription = ""
-                        )
-                    }
-
-                    val page = if (actualPage < 9) "0${actualPage + 1}" else "${actualPage + 1}"
-
-                    Row(
-                            modifier = Modifier.weight(4f),
-                            horizontalArrangement = Arrangement.Center,
-                            verticalAlignment = Alignment.CenterVertically
-                    ){
-                        Text(
-                                "Page $page sur $pageSize"
-                        )
-                    }
-
-                    IconButton(
-                            onClick = { actualPage++ },
-                            modifier = Modifier
-                                .weight(1.5f),
-                            enabled = actualPage != pageSize - 1
-                    ) {
-                        Icon(
-                                Icons.Rounded.KeyboardArrowRight,
-                                contentDescription = ""
-                        )
-                    }
-
-                    IconButton(
-                            onClick = { actualPage = pageSize - 1},
-                            modifier = Modifier
-                                .weight(1.5f),
-                            enabled = actualPage != pageSize - 1
-                    ) {
-                        Icon(
-                                Icons.Rounded.SkipNext,
-                                contentDescription = ""
-                        )
-                    }
-
-                }
+                BottomNavBar(
+                        actualPage = if(searchKey.isEmpty()) actualPage else actualPageWithSearch,
+                        pageSize = pageSize,
+                        updateActualPage = {
+                            if (searchKey.isEmpty())
+                                actualPage = it
+                            else
+                                actualPageWithSearch = it
+                        }
+                )
             }
     )
 
 }
 
+
 @Composable
-fun TopAppBar() {
+fun TopAppBar(
+    updateActualPageWithSearch: (Int) -> Unit ,
+    updateSearchKey: (String) -> Unit ,
+    searchKey: String ,
+) {
     Column(modifier = Modifier.background(Color.White)) {
 
         Row(
@@ -149,50 +103,91 @@ fun TopAppBar() {
                 verticalAlignment = Alignment.CenterVertically
         ) {
 
-            IconButton(
-                    onClick = { } ,
-                    modifier = Modifier
-                        .weight(0.15f)
-            ) {
-                Icon(
-                        Icons.Rounded.Menu ,
-                        contentDescription = "Menu" ,
-                        modifier = Modifier
-                            .size(24.dp)
-                )
-            }
+            var isSearching by remember { mutableStateOf(false) }
 
-            Text(
-                    "Countries list" ,
-                    style = MaterialTheme.typography.headlineSmall ,
-                    modifier = Modifier
-                        .weight(0.65f)
-            )
-
-            IconButton(
-                    onClick = { } ,
-                    modifier = Modifier
-                        .weight(0.15f)
-            ) {
-                Icon(
-                        Icons.Rounded.Search ,
-                        contentDescription = "Menu" ,
+            if (isSearching) {
+                IconButton(
+                        onClick = {
+                            updateActualPageWithSearch(0)
+                            updateSearchKey("")
+                            isSearching = !isSearching
+                        } ,
                         modifier = Modifier
-                            .size(24.dp)
-                )
-            }
+                            .weight(0.15f)
+                ) {
+                    Icon(
+                            Icons.Rounded.ArrowBack ,
+                            contentDescription = "Menu" ,
+                            modifier = Modifier
+                                .size(24.dp)
+                    )
+                }
 
-            IconButton(
-                    onClick = { } ,
-                    modifier = Modifier
-                        .weight(0.15f)
-            ) {
-                Icon(
-                        Icons.Rounded.MoreVert ,
-                        contentDescription = "Menu" ,
+                var typedText by remember { mutableStateOf(TextFieldValue(text = searchKey)) }
+
+                TextField(
+                        value = typedText ,
+                        onValueChange = { typingText ->
+                            typedText = typingText
+                        },
                         modifier = Modifier
-                            .size(24.dp)
+                            .weight(0.85f),
+                        colors = TextFieldDefaults.textFieldColors(
+                                containerColor = SoftGray,
+                                cursorColor = Color.Black
+                        ),
+                        singleLine = true,
+                        placeholder = { Text("Search...") }
                 )
+
+               updateSearchKey(typedText.text)
+
+            } else {
+                IconButton(
+                        onClick = { } ,
+                        modifier = Modifier
+                            .weight(0.15f)
+                ) {
+                    Icon(
+                            Icons.Rounded.Menu ,
+                            contentDescription = "Menu" ,
+                            modifier = Modifier
+                                .size(24.dp)
+                    )
+                }
+
+                Text(
+                        "Countries list" ,
+                        style = MaterialTheme.typography.headlineSmall ,
+                        modifier = Modifier
+                            .weight(0.65f)
+                )
+
+                IconButton(
+                        onClick = { isSearching = !isSearching } ,
+                        modifier = Modifier
+                            .weight(0.15f)
+                ) {
+                    Icon(
+                            Icons.Rounded.Search ,
+                            contentDescription = "Menu" ,
+                            modifier = Modifier
+                                .size(24.dp)
+                    )
+                }
+
+                IconButton(
+                        onClick = { } ,
+                        modifier = Modifier
+                            .weight(0.15f)
+                ) {
+                    Icon(
+                            Icons.Rounded.MoreVert ,
+                            contentDescription = "Menu" ,
+                            modifier = Modifier
+                                .size(24.dp)
+                    )
+                }
             }
 
         }
@@ -206,33 +201,33 @@ fun TopAppBar() {
 
 @Composable
 private fun BodyList(
-    innerPadding: PaddingValues ,
-    countryModel: CountryViewModel ,
+    innerPadding: PaddingValues,
     countriesState: CountryState ,
     navController: NavController,
-    actualPage: Int
+    actualPage: Int,
+    searchKey: String,
+    updatePageSize: (Int) -> Unit
 ) {
     LazyColumn(contentPadding = innerPadding) {
 
-        countryModel.getAll()
-
         when (countriesState) {
 
-            is CountryState.Loading -> item { Text("Loading") }
+            is CountryState.Loading -> item {
+                Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(4.dp),
+                        horizontalArrangement = Arrangement.Center,
+                ){ CircularProgressIndicator() } }
 
             is CountryState.Success.SuccessCountries -> {
 
-                val countries = countriesState.countries
-
-                val printedCountries = countries.subList(
-                        actualPage * 9,
-                        minOf(
-                                (actualPage + 1) * 9,
-                                countries.size
-                        )
-                )
-
-                items(printedCountries) { country ->
+                items(fetchData(
+                        countries = countriesState.countries,
+                        actualPage = actualPage,
+                        searchKey = searchKey,
+                        updatePageSize = { updatePageSize(it) }
+                )) { country ->
 
                     var isExpanded by remember { mutableStateOf(false) }
 
@@ -257,7 +252,7 @@ private fun BodyList(
                                     modifier = Modifier
                                         .size(16.dp)
                                         .padding(
-                                                vertical = 16.dp,
+                                                vertical = 16.dp ,
                                                 horizontal = 8.dp
                                         )
                                         .clip(CircleShape)
@@ -358,6 +353,35 @@ private fun BodyList(
 
 
     }
+}
+
+private fun fetchData(
+    countries : List<CountriesData> ,
+    actualPage: Int ,
+    searchKey: String ,
+    updatePageSize: (Int) -> Unit
+) : List<CountriesData> {
+
+    val filterCountries = if (searchKey.isEmpty()) {
+        countries
+    }
+    else {
+
+        countries.filter {
+            it.name.common.contains(searchKey, true)
+        }
+    }
+
+    updatePageSize((filterCountries.size + 8) / 9)
+
+    return filterCountries.subList(
+            actualPage * 9 ,
+            minOf(
+                    (actualPage + 1) * 9 ,
+                    filterCountries.size
+            )
+    )
+
 }
 
 @Composable
@@ -582,6 +606,91 @@ private fun ExpandablePart(
                     modifier = Modifier
                         .weight(0.1f)
                         .padding(8.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun BottomNavBar(
+    actualPage: Int,
+    pageSize: Int,
+    updateActualPage: (Int) -> Unit
+) {
+    Row(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxWidth() ,
+            verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        IconButton(
+                onClick = {
+                    updateActualPage(0)
+                } ,
+                modifier = Modifier
+                    .weight(1.5f) ,
+                enabled = actualPage != 0
+        ) {
+            Icon(
+                    Icons.Rounded.SkipPrevious ,
+                    contentDescription = "" ,
+            )
+        }
+
+        IconButton(
+                onClick = {
+                    updateActualPage(actualPage - 1)
+                } ,
+                modifier = Modifier
+                    .weight(1.5f) ,
+                enabled = actualPage != 0
+        ) {
+            Icon(
+                    Icons.Rounded.KeyboardArrowLeft ,
+                    contentDescription = ""
+            )
+        }
+
+        val page = if (actualPage < 9) "0${actualPage + 1}" else "${actualPage + 1}"
+        val totalPages = if (pageSize < 10) "0${pageSize}" else "${pageSize}"
+
+        Row(
+                modifier = Modifier.weight(4f) ,
+                horizontalArrangement = Arrangement.Center ,
+                verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                    "Page $page sur $totalPages"
+            )
+        }
+
+        IconButton(
+                onClick = {
+                    updateActualPage(actualPage + 1)
+                } ,
+                modifier = Modifier
+                    .weight(1.5f) ,
+                enabled = actualPage != pageSize - 1
+        ) {
+            Icon(
+                    Icons.Rounded.KeyboardArrowRight ,
+                    contentDescription = ""
+            )
+        }
+
+        IconButton(
+                onClick = {
+                    updateActualPage( pageSize - 1)
+                } ,
+                modifier = Modifier
+                    .weight(1.5f) ,
+                enabled = actualPage != pageSize - 1
+        ) {
+            Icon(
+                    Icons.Rounded.SkipNext ,
+                    contentDescription = ""
             )
         }
 
