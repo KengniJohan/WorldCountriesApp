c3d5b81bca5784b90d2db6d58a92b55e4a7f33c0
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-02 16:24:51 +0100

    Modification du type de retour de la methode getAll()

--- app/src/main/java/com/android/mycountryapp/repository/CountryRepository.kt
+++ app/src/main/java/com/android/mycountryapp/repository/CountryRepository.kt
@@ -3,14 +3,13 @@ package com.android.mycountryapp.repository
 import com.android.mycountryapp.data.CountryDataApiService
 import com.android.mycountryapp.data.models.CountriesData
 import com.android.mycountryapp.data.models.Country
-import retrofit2.Response
 import javax.inject.Inject
 
 class CountryRepository @Inject constructor(
     private val countryService: CountryDataApiService
     )
 {
-    suspend fun getAll() : Response<List<CountriesData>> = countryService.getAll()
+    suspend fun getAll() : List<CountriesData> = countryService.getAll()
 
     suspend fun getByName(countryName: String) : List<Country> = countryService.getByName(countryName)
 
